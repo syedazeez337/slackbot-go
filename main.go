ea3766879702e0775cad2ad12c4d26ee45c451bd
Syed Azeez <syedazeez337@gmail.com>
2024-11-07 21:39:28 +0530

    New

---
+++ main.go
@@ -0,0 +1,48 @@
+/package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	"github.com/shomali11/slacker"
+)
+
+func main() {
+
+	bot := slacker.NewClient(os.Getenv("SLACK_BOT_TOKEN"), os.Getenv("SLACK_APP_TOKEN"))
+
+	go printCommandEvents(bot.CommandEvents())
+	bot.Command("ping", &slacker.CommandDefinition{
+		Handler: func(botCtx slacker.BotContext, request slacker.Request, respose slacker.ResponseWriter) {
+			respose.Reply("pong")
+		},
+	})
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	err := bot.Listen(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func printCommandEvents(analyticsChannel <-chan *slacker.CommandEvent) {
+	for event := range analyticsChannel {
+		fmt.Println("Command Events")
+		fmt.Println(event.Timestamp)
+		fmt.Println(event.Command)
+		fmt.Println(event.Parameters)
+		fmt.Println(event.Event)
+		fmt.Println()
+	}
+}
+
+func init() {
+	if err := godotenv.Load(); err != nil {
+		fmt.Println("No .env file found")
+	}
+}
